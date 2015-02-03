fbe576df5c6c65683946aa966eaa3273ffce5f14
Vibhav Pant <vibhavp@gmail.com>
2015-02-03 16:58:17 +0530

    Add license.

--- erc-social-graph.el
+++ erc-social-graph.el
@@ -1,3 +1,23 @@
+;;; erc-social-graph.el --- A social network graph module for ERC.
+
+;;; Copyright (C) 2014 Vibhav Pant <vibhavp@gmail.com>
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
 (require 'erc)
 
 (defun sgraph-update-hash ()

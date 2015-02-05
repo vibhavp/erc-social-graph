e3d560066b17a005983dc1e035610bb480ad6b88
Vibhav Pant <vibhavp@gmail.com>
2015-02-05 09:54:47 +0530

    Split updating work between two functions, add function for drawing graph.

--- erc-social-graph.el
+++ erc-social-graph.el
@@ -20,36 +20,67 @@
 ;;; Code:
 (require 'erc)
 
-(defun sgraph-update-hash ()
-  "Update graph"
-  (let ((text (buffer-substring-no-properties (point-min) (point-max))))
+(defcustom sgraph-update-function 'sgraph-update-graph
+  "The function used for updating the graph hash table. Takes the message
+ sender as an argument."
+  :type 'function
+  :group 'erc-social-graph)
+
+(defvar graph (make-hash-table :test 'equal))
+
+(defun sgraph-update-graph (sender text)
+  "Update the graph"
+  (maphash (lambda (nick value)
+	     (let ((result (ignore-errors (string-match (format "\\<%s\\>\\([^[:alpha:]]\\|$\\)"
+								(downcase nick))
+							(downcase text)))))
+	       (when result
+		 (let* ((key (concat (downcase sender) "-" (downcase nick)))
+			(times (gethash key graph nil)))
+		   (if (eq times nil)
+		       (puthash key 1 graph)
+		     (puthash key (+ times 1) graph))))))
+	   erc-channel-users)) 
+
+(defun sgraph-update ()
+  "Parse the text in the current buffer, and pass it to sgraph-update-function"
+  (let ((text (buffer-substring-no-properties (point-min) (point-max))))    
     (when (string=  (substring text 0 1) "<")
       (let ((sender (substring text (+ 1 (string-match "<" text))
 				(string-match ">" text))))
 	(setq text (substring text (+ 2 (string-match ">" text))))
-	(maphash (lambda (nick value)
-		   (let ((result (ignore-errors (string-match nick text))))
-		     (when result
-		       (let* ((key (concat sender "-" nick))
-			      (times (gethash key graph nil)))
-			 (unless (string= sender nick)
-			   (if (not times)
-			       (puthash key 1 graph)
-			     (puthash key (+ times 1) graph)))))))
-		 erc-channel-users)))))    
+	(funcall sgraph-update-function sender text)))))  
 
 (defun sgraph-create ()
-  "Greate graph"
-  (setq-default graph (make-hash-table :test 'equal))
-  (make-variable-buffer-local 'graph))
+  "Create graph"
+  (make-local-variable 'graph))
 
-;;;###autoload(autoload 'erc-social-graph-mode "erc-social-graph" nil t)
+(defun sgraph-draw (channel)
+  "Draw a graph for the input channel"
+  (interactive (list (completing-read "Draw graph for channel: "
+				      (mapcar 'buffer-name (erc-buffer-list)))))
+  (let ((buffer (concat "graph-" channel)))
+    (get-buffer-create buffer)
+    (set-buffer buffer)
+    (insert "digraph {")
+    (maphash (lambda (link value)
+	       (insert (format "\"%s\" -> \"%s\" [penwidth = %d]; \n"
+			       (substring link 0 (string-match "-" link))
+			       (substring link (+ 1 (string-match "-" link)))
+			       (if (> value 6)
+				   6
+				 value))))
+	     graph)
+    (insert "}")
+    (switch-to-buffer buffer)))
+
 (define-erc-module social-graph nil
   "Social network gaphs similar to piespy"
   ;; Enable
   ((add-hook 'erc-join-hook 'sgraph-create)
-   (add-hook 'erc-insert-post-hook 'sgraph-update-hash))
+   (add-hook 'erc-insert-post-hook 'sgraph-update))
   ;; Disable
-  ((makunbound graph)
-   (remove-hook 'erc-insert-post-hook 'sgraph-update-hash)
+  ((remove-hook 'erc-insert-post-hook 'sgraph-update-hash)
    (remove-hook 'erc-join-hook 'sgraph-create)))
+
+(provide 'erc-social-graph)
